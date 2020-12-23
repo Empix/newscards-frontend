5fcbb2365bf87311f5ce2a04f4e6252b69595da3
Empix <jotinhave@gmail.com>
2020-12-22 23:04:23 -0400

    Create Header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './style.css';
+
+export default function Header() {
+  return (
+    <header>
+      <h1 className="title">NewsCards</h1>
+    </header>
+  );
+}
