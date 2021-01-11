067dc9c4ef02b1dd1474b120c9ac41983417c219
Empix <jotinhave@gmail.com>
2021-01-11 02:48:26 -0400

    Link the logo to home page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import './style.css';
 
 export default function Header() {
+  const history = useHistory();
+
   return (
     <header>
-      <h1 className="title">NewsCards</h1>
+      <h1 className="title" onClick={() => history.push('/')}>
+        NewsCards
+      </h1>
     </header>
   );
 }
