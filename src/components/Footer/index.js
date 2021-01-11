de55ce1c4c2a8007e615cf3cc748c53bdee00dbf
Empix <jotinhave@gmail.com>
2021-01-11 02:09:11 -0400

    Create footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './style.css';
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Feito com 🤍 por{' '}
+        <a href="https://github.com/empix" target="_blank" rel="noreferrer">
+          Empix
+        </a>
+      </p>
+    </footer>
+  );
+}
