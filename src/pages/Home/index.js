3b57569f8728ff441da641c1ecfc5b8f4f528cc1
Empix <jotinhave@gmail.com>
2021-02-16 11:45:09 -0400

    Create search form

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
+import { IoMdSearch } from 'react-icons/io';
+
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import Card from '../../components/Card';
@@ -11,6 +13,7 @@ import api from '../../services/api';
 
 export default function Home() {
   const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     api
@@ -30,12 +33,36 @@ export default function Home() {
     setData(newData);
   }
 
+  function handleSearch(event) {
+    event.preventDefault();
+
+    api
+      .get('newscards', { params: { search } })
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        alert('An unknown error has occurred!');
+      });
+  }
+
   return (
     <div className="container">
       <Header />
 
       <div className="tool-bar">
-        <input type="text" placeholder="Search" />
+        <form onSubmit={handleSearch} className="search">
+          <input
+            type="text"
+            placeholder="Search"
+            value={search}
+            onChange={({ target }) => setSearch(target.value)}
+          />
+          <button type="submit" className="button">
+            <IoMdSearch />
+          </button>
+        </form>
+
         <button className="button" onClick={() => history.push('/new')}>
           NEW
         </button>
