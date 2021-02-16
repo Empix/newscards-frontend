dbdd5a9caae4ff325601e04da316b1f906ae4731
Empix <jotinhave@gmail.com>
2021-02-16 01:16:13 -0400

    Add feature edit card

---
+++ src/pages/Edit/index.js
@@ -0,0 +1,153 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import { IoMdArrowBack, IoMdImage } from 'react-icons/io';
+
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+import './style.css';
+
+import api from '../../services/api';
+
+export default function Edit() {
+  const { id } = useParams();
+  const history = useHistory();
+
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [link, setLink] = useState('');
+  const [description, setDescription] = useState('');
+
+  const [imageFile, setImageFile] = useState(null);
+  const [imagePreview, setImagePreview] = useState('');
+
+  useEffect(() => {
+    api
+      .get(`newscards/${id}`)
+      .then(({ data }) => {
+        setTitle(data.title);
+        setAuthor(data.author);
+        setLink(data.url);
+        setDescription(data.description);
+        setImagePreview(data.image_url);
+      })
+      .catch((error) => {
+        alert('An unknown error has occurred!');
+      });
+  }, [id]);
+
+  function handleImageInputChange(event) {
+    const file = event.target.files[0];
+
+    if (!file) return;
+
+    if (file.size > 2 * 1024 * 1024) {
+      return alert('Too big file!');
+    }
+
+    setImageFile(file);
+  }
+
+  useEffect(() => {
+    if (!imageFile) return;
+
+    const objectURL = URL.createObjectURL(imageFile);
+    setImagePreview(objectURL);
+
+    return () => URL.revokeObjectURL(objectURL);
+  }, [imageFile]);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append('title', title);
+    formData.append('author', author);
+    formData.append('url', link);
+    formData.append('description', description);
+    formData.append('image', imageFile);
+
+    api
+      .put(`newscards/${id}`, formData)
+      .then((response) => {
+        alert('News card successfully edited!');
+        history.push('/');
+      })
+      .catch((error) => {
+        alert('An unknown error has occurred!');
+      });
+  }
+
+  return (
+    <div className="container">
+      <Header />
+
+      <div className="bar">
+        <div className="back-to-home" onClick={() => history.push('/')}>
+          <IoMdArrowBack />
+          GO BACK
+        </div>
+      </div>
+
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <label
+            className="image-label"
+            htmlFor="image"
+            style={{ backgroundImage: `url(${imagePreview})` }}
+          >
+            <p>
+              <IoMdImage />
+              Choose an image
+            </p>
+          </label>
+          <input
+            type="file"
+            name="image"
+            id="image"
+            onChange={handleImageInputChange}
+            accept="image/jpeg, image/pjpeg, image/png"
+          />
+
+          <input
+            required
+            type="text"
+            placeholder="Title"
+            name="title"
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+          <input
+            required
+            type="text"
+            placeholder="Author"
+            name="author"
+            value={author}
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+          <input
+            required
+            type="url"
+            placeholder="Link"
+            name="link"
+            value={link}
+            onChange={({ target }) => setLink(target.value)}
+          />
+          <textarea
+            required
+            placeholder="Description"
+            name="description"
+            value={description}
+            onChange={({ target }) => setDescription(target.value)}
+          ></textarea>
+
+          <button type="submit" className="button">
+            UPDATE
+          </button>
+        </form>
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/Home';
 import New from './pages/New';
+import Edit from './pages/Edit';
 
 export default function Routes() {
   return (
@@ -10,6 +11,7 @@ export default function Routes() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/new" component={New} />
+        <Route path="/edit/:id" component={Edit} />
       </Switch>
     </BrowserRouter>
   );
